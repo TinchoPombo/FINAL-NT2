3b8abd7959812295c9a4c4819c4d1d046a5adcdb
TinchoPombo <martinpombo21@gmail.com>
2022-07-12 20:18:11 -0300

    first commit

---
+++ src/components/Inicio.vue
@@ -0,0 +1,105 @@
+<template>
+
+  <section class="src-components-inicio">
+    <div class="jumbotron">
+      <h1>FINAL</h1>
+      <hr>
+      <hr>
+
+      Color: <input type="text" v-model="color">
+      Fondo: <input type="text" v-model="fondo">
+      Tamaño: <input type="text" v-model="tamanio">
+      <hr>
+      Texto: <input type="text" v-model="texto" :style="getStyle">
+
+      <Palabras v-show="texto" :texto="texto"/>
+
+      <br>
+      <br>
+      <hr>
+      <table class="table table-dark">
+        <tr>
+          <th>Pregunta</th>
+          <th>Respuesta</th>
+        </tr>
+        <tr v-for="(pregunta,index) in preguntas" :key="index">
+          <td>{{ pregunta.pregunta }}</td>
+          <td>{{ pregunta.respuesta }}</td>
+        </tr>
+      </table>
+    </div>
+  </section>
+
+</template>
+
+<script>
+
+  import Palabras from './Palabras.vue'
+  export default  {
+    name: 'src-components-inicio',
+    components: {
+      Palabras
+    },
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+        texto: '',
+        color: '',
+        fondo: '',
+        tamanio: '',
+
+
+
+        preguntas: [
+          {
+            pregunta:'1',
+            respuesta:'B'
+          },
+
+          {
+            pregunta:'2',
+            respuesta:'C'
+          },
+          
+          {
+            pregunta:'3',
+            respuesta:'B'
+          },
+
+          {
+            pregunta:'4',
+            respuesta:'D'
+          },
+
+          {
+            pregunta:'5',
+            respuesta:'D'
+          }
+        ]
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+      getStyle(){
+      return { 
+        'color': this.color,
+        'background-color': this.fondo,
+        'font-size': this.tamanio
+      }
+    }
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-inicio {
+
+  }
+</style>
---
+++ src/components/Palabras.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <section class="src-components-palabras">
+    Cantidad de Palabras: {{obtenerCantPalabras}}
+  </section>
+
+</template>
+
+<script>
+
+  export default  {
+    name: 'src-components-palabras',
+    props: ['texto'],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+      obtenerCantPalabras(){
+        return this.texto.trim().split(' ').length
+      }
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-palabras {
+
+  }
+</style>
